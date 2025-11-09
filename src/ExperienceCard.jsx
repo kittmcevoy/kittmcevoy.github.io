9eb18e5be46070706b5468abf7afd58fe78fe6e3
kittmcevoy <kitt.mcevoy@outlook.com>
2025-11-09 21:42:34 +1300

    Made Experience page more reactive to screensize adjustments.

--- src/ExperienceCard.jsx
+++ src/ExperienceCard.jsx
@@ -4,16 +4,20 @@ function ExperienceCard({ jobTitle, company, dates, logo, bgColor, JobDescriptio
   return (
     <div className={`${bgColor}`}>
       <div className="flex flex-row justify-left ml-8 pt-6">
-        <img src={logo} alt={`${company} logo`} className="mr-28 w-32 h-auto object-contain " />
-        <div className="flex flex-col">
-          <div className="flex flex-row">
-            <div className="text-xl">Job Title: {jobTitle} </div>
-            <div className="text-xl px-4"> | </div>
-            <div className="text-xl">Company: {company}</div>
-            <div className="text-xl px-4"> | </div>
-            <div className="text-xl">{dates}</div>
+        <img src={logo} alt={`${company} logo`} className="mr-20 w-32 h-auto object-contain md:flex hidden" />
+        <div className="flex flex-col mr-2">
+          <div className="justify-between flex flex-row">
+            <div className="flex flex-col md:flex-row">
+              <div className="text-xl">Job Title: {jobTitle} </div>
+              <div className="text-xl px-4 hidden md:flex"> | </div>
+              <div className="text-xl">Company: {company}</div>
+              <div className="text-xl px-4 hidden md:flex"> | </div>
+              <div className="text-xl">{dates}</div>
+            </div>
+            <img src={logo} alt={`${company} logo`} className="mx-2 w-32 h-auto object-contain flex md:hidden" />
           </div>
-          <div className="whitespace-pre-line w-5/6">
+
+          <div className="whitespace-pre-line lg:w-5/6">
             {JobDescription}
           </div>
         </div>
