3fcd1aa04c337b360ed852fa734d7ab62d464508
kittmcevoy <kitt.mcevoy@outlook.com>
2025-11-09 21:49:22 +1300

    Added VUW job to experience.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,9 +3,10 @@ import ExperienceCard from './ExperienceCard.jsx'
 import TenzingLogo from './images/tenzing-logo.png'
 import GeeksLogo from './images/geeks-logo.png'
 import CricHQLogo from './images/CricHQ-logo.png'
+import VUWLogo from './images/VUW-logo.png'
 
 
-const GeeksJobDescription = `
+const GeeksJobDescription=`
 In this role I actively use skills in troubleshooting, customer service and sales, with a commitment to continuous learning and technical upskilling, to give customers the best possible experience with technology.
 •	Software and hardware troubleshooting on PC and Mac.
 •	Installing and maintaining small business and home networks. 
@@ -23,6 +24,11 @@ In this role, I actively contributed to the development and maintenance of full-
 
 `
 
+const VUWJobDescription=`
+I tutored NWEN243 in the Computer Science program, offering comprehensive sessions on networking concepts, aiding problem-solving, and evaluating assignments with constructive feedback for student academic growth.
+
+`
+
 function Experience() {
   return (
     <div>
@@ -30,32 +36,41 @@ function Experience() {
         <Header />
       </div>
       <div className="pt-20">
-        <ExperienceCard 
-          jobTitle="DevOps Engineer" 
-          company="Tenzing" 
-          dates="September 2024 - Present" 
-          logo={TenzingLogo} 
+        <ExperienceCard
+          jobTitle="DevOps Engineer"
+          company="Tenzing"
+          dates="September 2024 - Present"
+          logo={TenzingLogo}
           bgColor={"bg-white"}
-          JobDescription={GeeksJobDescription} 
-          />
-
-        <ExperienceCard 
-          jobTitle="Mobile technician" 
-          company="Geeks On Wheels" 
-          dates="February 2024 - September 2024" 
-          logo={GeeksLogo} 
-          bgColor={"bg-slate-200"} 
           JobDescription={GeeksJobDescription}
-          />
+        />
 
-           <ExperienceCard 
-          jobTitle="Intern Software Engineer" 
-          company="CricHQ" 
-          dates="November 2021 - November 2023" 
-          logo={CricHQLogo} 
+        <ExperienceCard
+          jobTitle="Mobile technician"
+          company="Geeks On Wheels"
+          dates="February 2024 - September 2024"
+          logo={GeeksLogo}
+          bgColor={"bg-slate-200"}
+          JobDescription={GeeksJobDescription}
+        />
+
+        <ExperienceCard
+          jobTitle="Intern Software Engineer"
+          company="CricHQ"
+          dates="November 2021 - November 2023"
+          logo={CricHQLogo}
           bgColor={"bg-white"}
           JobDescription={CricHQJobDescription}
-          />
+        />
+
+        <ExperienceCard
+          jobTitle="Tutor"
+          company="Victoria University of Wellington "
+          dates="2022 - 2023"
+          logo={VUWLogo}
+          bgColor={"bg-slate-200"}
+          JobDescription={VUWJobDescription}
+        />
 
       </div>
     </div >
